ab7f3d8a43601272b79ea1236a978a1a2e874551
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-20 04:30:12 +0200

    feat: implement password generation functionality and update HTML structure

--- index.js
+++ index.js
@@ -1,6 +1,115 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
 
+const buttonEl = document.getElementById("generate-el");
+const passwordEl1 = document.getElementById("password1");
+const passwordEl2 = document.getElementById("password2");
 
+const passwordLength = 15;
+function generatePasswords() {
+  let password1 = "";
+  let password2 = "";
 
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex1 = Math.floor(Math.random() * characters.length);
+    const randomIndex2 = Math.floor(Math.random() * characters.length);
+    password1 += characters[randomIndex1];
+    password2 += characters[randomIndex2];
+  }
 
+  passwordEl1.textContent = password1;
+  passwordEl2.textContent = password2;
+}
+
+buttonEl.addEventListener("click", generatePasswords);
